c6100ee7617a974ca4c44bf1ff18da387a97fca8
217heidai <217heidai@gmail.com>
2024-07-16 12:35:39 +0800

    update

--- mpconfigboard.cmake
+++ mpconfigboard.cmake
@@ -2,5 +2,6 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     ${SDKCONFIG_IDF_VERSION_SPECIFIC}
     boards/sdkconfig.ble
-    boards/sdkconfig.custom
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.spiram
 )
