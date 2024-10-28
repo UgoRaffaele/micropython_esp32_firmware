175d02f3d637edb8ebbc1e3d55472ff98bfe2014
217heidai <217heidai@gmail.com>
2024-10-28 16:03:13 +0800

    update

--- ESP32-WROVER-B-N4R8/micropython.cmake
+++
@@ -1,9 +0,0 @@
-set(SDKCONFIG_DEFAULTS
-    ${SDKCONFIG_DEFAULTS}
-    boards/sdkconfig.240mhz
-    boards/sdkconfig.spiram
-)
-
-list(APPEND MICROPY_DEF_BOARD
-    MICROPY_HW_BOARD_NAME="Generic ESP32 module with SPIRAM"
-)
